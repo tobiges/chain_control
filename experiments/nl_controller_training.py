4813b04ee789f7513db5aa284b9ff820877fad3a
Tobias Gessler <gessler.tobi@gmail.com>
2022-12-16 08:56:29 +0100

    feat: many cool images and more ✌️

--- experiments/nl_controller_training.py
+++ experiments/nl_controller_training.py
@@ -8,7 +8,7 @@ from cc.env.wrappers import AddRefSignalRewardFnWrapper, RecordVideoWrapper
 from cc.examples.neural_ode_controller_compact_example import make_neural_ode_controller
 from cc.examples.pid_controller import make_pid_controller
 from cc.train import *
-from experiments.helpers import plot_analysis
+from experiments.helpers import calc_reward, plot_analysis
 import jax.random as jrand
 from cc.utils import rmse, l2_norm
 import optax
@@ -18,25 +18,26 @@ import equinox as eqx
 import pprint
 import numpy
 import matplotlib.pyplot as plt 
+import jax as jax
 
 pp = pprint.PrettyPrinter(indent=4)
 numpy.set_printoptions(threshold=5)
 
 ###### Parameters#################
 
-env_num = 1
+env_num = 2
 
-ref = 20
+ref = list(range(5000))
 
 constant_after = 3.0
 
-iterations = 1
+iterations = 500
 
-state_dim = 5
-f_width_size = 1
-f_depth = 25
-g_width_size = 25
-g_depth = 10
+state_dim = 80
+f_width_size = 0
+f_depth = 0
+g_width_size = 0
+g_depth = 0
 
 # Baseline:
 # state_dim = 5
@@ -47,7 +48,7 @@ g_depth = 10
 
 ########################
 
-env1 = make_env(f"two_segments_v1", random=1, time_limit=10.0, control_timestep=0.01)
+env1 = make_env(f"two_segments_v{env_num}", random=1, time_limit=10.0, control_timestep=0.01)
 
 
 model1 = make_neural_ode_model(
@@ -60,10 +61,12 @@ model1 = make_neural_ode_model(
 )
 
 model1 = eqx.tree_deserialise_leaves(
-    "/data/ba54womo/chain_control/experiments/models/good_env1_model2.eqx", model1)
+    f"/data/ba54womo/chain_control/experiments/models/good_env{env_num}_model2.eqx", model1)
 
-source1, _ = sample_feedforward_collect_and_make_source(env1, seeds=[ref])
+#source1, _ = sample_feedforward_collect_and_make_source(env1, seeds=[ref])
 #source1 = constant_after_transform_source(source1, after_T = constant_after, new_ts = env1.ts)
+
+source1 = collect_random_step_source(env1, seeds=ref, amplitude=5.0)
 
 env1_w_source = AddRefSignalRewardFnWrapper(env1, source1)
 
@@ -78,10 +81,25 @@ controller1 = make_neural_ode_controller(
     g_width_size=g_width_size,
     g_depth=g_depth,
 )
+
+upper_bound = 3.0
+lower_bound = -upper_bound
+
+@jax.vmap
+def _random_step(ref, key):
+    return jnp.ones_like(ref) * jrand.uniform(
+        key, (), minval=lower_bound, maxval=upper_bound
+    )
+
+def tree_transform(key, ref, bs):
+    keys = jrand.split(key, bs)
+    return jtu.tree_map(lambda ref: _random_step(ref, keys), ref)
+
 controller_dataloader1 = make_dataloader(
     UnsupervisedDataset(source1.get_references_for_optimisation()),
     jrand.PRNGKey(1,),
-    n_minibatches=1
+    n_minibatches=5,
+    # tree_transform=tree_transform,
 )
 optimizer1 = optax.chain(optax.clip_by_global_norm(1.0), optax.adam(1e-3))
 controller_train_options1 = TrainingOptionsController(
@@ -96,9 +114,8 @@ controller_trainer1.run(iterations)
 
 fitted_controller1 = controller_trainer1.trackers[0].best_model_or_controller()
 # env1_w_video = RecordVideoWrapper(env1_w_source, width=1280, height=720, cleanup_imgs=False)
-controller_performance_sample1 = collect_exhaust_source(env1_w_source, fitted_controller1)
 
-plot_analysis(fitted_controller1, env1, model1, "images/plot_removeme.png")
+plot_analysis(fitted_controller1, env1, model1, f"images/plot_env{env_num}_{len(ref)}ref_{iterations}it.png", f"two_segments_v{env_num}")
 
 #plt.plot(controller_performance_sample1.obs["obs"]["xpos_of_segment_end"][0], label="obs")
 #plt.plot(controller_performance_sample1.obs["ref"]["xpos_of_segment_end"][0], label="reference")
---
+++ experiments/pid_manual.py
@@ -0,0 +1,110 @@
+from typing import Any
+from experiments.helpers import calc_reward, get_eval_source, plot_analysis
+
+import numpy as np
+import ray
+from ray import air, tune
+
+from cc.config import disable_compile_warn, disable_tqdm
+
+
+# from .neural_ode_controller import make_neural_ode_controller
+import equinox as eqx
+from cc.env import *
+from cc.env.collect import collect, collect_exhaust_source,  sample_feedforward_collect_and_make_source
+from cc.env.collect.source import *
+from cc.env.wrappers import AddRefSignalRewardFnWrapper
+from cc.examples.pid_controller import make_pid_controller
+from cc.train import *
+import jax.random as jrand
+from cc.utils import rmse, l2_norm
+import jax
+import optax
+import jax.numpy as jnp
+from cc.examples.neural_ode_model_compact_example import make_neural_ode_model
+import pprint
+import numpy
+from cc.env.collect.collect import append_source, collect_random_step_source
+
+import matplotlib.pyplot as plt 
+
+
+
+def force_cpu_backend():
+    from jax import config
+
+    config.update("jax_platform_name", "cpu")
+    config.update("jax_platforms", "cpu")
+
+
+def train_and_judge(config) -> float:
+    env = make_env("two_segments_v1", random=1, time_limit=10.0, control_timestep=0.01)
+
+    model = make_neural_ode_model(
+        env.action_spec(),
+        env.observation_spec(),
+        env.control_timestep,
+        state_dim=100,
+        f_depth=0,
+        u_transform=jnp.arctan
+    )
+
+    model = eqx.tree_deserialise_leaves(
+        "/data/ba54womo/chain_control/experiments/models/good_env1_model2.eqx", model)
+
+    source = collect_random_step_source(env, seeds=list(range(100)), amplitude=5.0)
+
+    #env_w_source = AddRefSignalRewardFnWrapper(env, source)
+
+    controller = make_pid_controller(config["p"], config["i"], config["d"], env.control_timestep)
+
+    upper_bound = 3.0
+    lower_bound = -upper_bound
+
+    @jax.vmap
+    def _random_step(ref, key):
+        return jnp.ones_like(ref) * jrand.uniform(
+            key, (), minval=lower_bound, maxval=upper_bound
+        )
+
+    def tree_transform(key, ref, bs):
+        keys = jrand.split(key, bs)
+        return jtu.tree_map(lambda ref: _random_step(ref, keys), ref)
+
+    controller_dataloader = make_dataloader(
+        UnsupervisedDataset(source.get_references_for_optimisation()),
+         jrand.PRNGKey(1,),
+         n_minibatches=5,
+         tree_transform=tree_transform,
+    )
+
+
+    controller_train_options = TrainingOptionsController(
+        controller_dataloader, optax.adam(3e-3), 
+    )
+
+    controller_trainer = ModelControllerTrainer(
+        model, controller, controller_train_options=controller_train_options, 
+        trackers=[Tracker("train_mse")]
+    )
+
+    controller_trainer.run(500)
+
+    fitted_controller = controller_trainer.trackers[0].best_model_or_controller()
+
+    eval_source = get_eval_source(5, 5, 5)
+    controller_performance_sample = collect_exhaust_source(
+        AddRefSignalRewardFnWrapper(env, eval_source), fitted_controller)
+
+    plot_analysis(fitted_controller, env, model, f"images/plot_state:p_{config['p']}_i:{config['i']}_d:{config['d']}.png", False)
+
+
+    return calc_reward(controller_performance_sample)
+
+config = {
+    "p": 1.0,
+    "i": 0.45,
+    "d": -0.67,
+}
+force_cpu_backend()
+print(train_and_judge(config))
