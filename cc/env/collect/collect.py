5417605a2081956a2d7ffb9d4f5a46986500976d
stormofice <58337328+stormofice@users.noreply.github.com>
2023-01-12 17:31:48 +0100

    feat: Include source append

--- cc/env/collect/collect.py
+++ cc/env/collect/collect.py
@@ -126,3 +126,12 @@ def collect_random_step_source(env: dm_env.Environment, seeds: list[int]):
     _yss["xpos_of_segment_end"] = yss
     _yss = BatchedTimeSeriesOfRef(_yss)
     return ObservationReferenceSource(_yss, ts=ts)
+
+def append_source(first: ObservationReferenceSource, second: ObservationReferenceSource):
+    
+    # Appends second yss after first yss without replacing
+    yss = OrderedDict()
+    for key in first._yss.keys():
+        yss[key] = np.concatenate([first._yss[key], second._yss[key]], axis=0)
+    
+    return ObservationReferenceSource(yss, ts=first._ts)
